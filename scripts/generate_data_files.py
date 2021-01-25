19dd5ac968c3022428a2bcec7d9da2a48334da3b
AmmarRabie <ammaralsayed55@gmail.com>
2021-01-25 02:46:43 +0200

    generate_dat script update .mb file with data_path

--- scripts/generate_data_files.py
+++ scripts/generate_data_files.py
@@ -7,7 +7,7 @@ import subprocess
 
 
 # converterLocation = "C:/Data/Others/progs/audiowaveform-1.4.2-win64/audiowaveform.exe"
-converterLocation = "audiowaveform.exe"
+converterLocation = "audiowaveform"
 def main():
     args = sys.argv
     if(len(args) != 2):
@@ -15,16 +15,24 @@ def main():
         return
     mbPath = args[1]
     with open(mbPath) as mbFile:
-        files = json.loads(mbFile.read())["project"]["files"]
-    paths = map(lambda f: f['path'], files)
-    for p in paths:
+        project = json.loads(mbFile.read())
+        files = project["project"]["files"]
+    # paths = map(lambda f: f['path'], files)
+    for f in files:
+        p = f['path']
         audioExt = p.split(".")[-1]
         dataPath = p.replace("." + audioExt, ".dat")
-        cmd = f'"{converterLocation}" -i "{p}" -o "{dataPath}" -z 256 -b 8'
+        cmd = f'"{converterLocation}" -i "{p}" -o "{dataPath}" -z 256 -b 16'
+        # TODO: use --pixels-per-second, so that we be indpendent of samplerate and number of frames..
+        # cmd = f'"{converterLocation}" -i "{p}" -o "{dataPath}" -b 16 --pixels-per-second 200'
+        # cmd = f'ffmpeg -i "{p}" -f wav pipe:1 | {converterLocation} --input-foramt wav --output-format dat -b 16 > "{dataPath}"'
         print("issuing", cmd)
         process = subprocess.run(cmd)
         process.check_returncode()
-        
+        f['data_path'] = dataPath
+    with open(mbPath, 'wt', encoding='utf-8') as resultFile:
+        resultFile.write(json.dumps(project))
+
     
 
 if __name__ == "__main__":
